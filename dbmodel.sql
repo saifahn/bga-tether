07f373c15bf110be6338505271220ae77bbf1152
Sean Li <9913851+saifahn@users.noreply.github.com>
2025-03-14 00:36:46 +0900

    feat: make db type_arg a string to support leading 0

--- dbmodel.sql
+++ dbmodel.sql
@@ -17,7 +17,7 @@
 CREATE TABLE IF NOT EXISTS `card` (
   `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `card_type` varchar(16) NOT NULL,
-  `card_type_arg` int(11) NOT NULL,
+  `card_type_arg` varchar(11) NOT NULL,
   `card_location` varchar(16) NOT NULL,
   `card_location_arg` int(11) NOT NULL,
   PRIMARY KEY (`card_id`)
