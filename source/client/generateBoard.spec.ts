1bc043bea06ff6f197662bd9fddde7f121fccf15
Sean Li <9913851+saifahn@users.noreply.github.com>
2025-03-20 18:58:45 +0900

    wip: add test and fn for creating board matrix

--- source/client/generateBoard.spec.ts
+++ source/client/generateBoard.spec.ts
@@ -9,4 +9,46 @@ gen('should return an empty array when no group is provided', () => {
   assert.equal(result, []);
 });
 
+gen(
+  'should return cards in the right order when a two card vertical group is supplied',
+  () => {
+    const result = generateGroup({
+      vertical: {
+        '01': { id: '1', number: '01', flipped: false },
+        '02': { id: '2', number: '02', flipped: false },
+      },
+    });
+    assert.equal(result, [
+      [{ id: '1', number: '01', flipped: false, uprightFor: 'vertical' }],
+      [{ id: '2', number: '02', flipped: false, uprightFor: 'vertical' }],
+    ]);
+  }
+);
+
+gen(
+  'should return cards in the right order when a two card horizontal group is supplied',
+  () => {
+    const result = generateGroup({
+      horizontal: {
+        '01': { id: '1', number: '01', flipped: false },
+        '02': { id: '2', number: '02', flipped: false },
+      },
+    });
+    assert.equal(result, [
+      { id: '1', number: '01', flipped: false, uprightFor: 'horizontal' },
+      { id: '2', number: '02', flipped: false, uprightFor: 'horizontal' },
+    ]);
+  }
+);
+
+gen(
+  'should return cards in the right order when a two card group of flipped cards is supplied',
+  () => {}
+);
+
+gen(
+  'should return cards in the right order when a two card group of mixed flip orientation is supplied',
+  () => {}
+);
+
 gen.run();
--- source/client/generateBoard.ts
+++ source/client/generateBoard.ts
@@ -1,14 +1,42 @@
 interface Group {
-  [cardNum: string]: {
-    id: string;
-    number: string;
-    flipped: boolean;
-    orientation: 'vertical' | 'horizontal';
+  vertical?: {
+    [cardNum: string]: {
+      id: string;
+      number: string;
+      flipped: boolean;
+    };
+  };
+  horizontal?: {
+    [cardNum: string]: {
+      id: string;
+      number: string;
+      flipped: boolean;
+    };
   };
 }
 
 export function generateGroup(group: Group) {
-  // output a matrix of cards? then we can use grid for each group
-  // let's add a test here
-  if (Object.keys(group).length === 0) return [];
+  const verticalCards = group.vertical && Object.values(group.vertical);
+  const horizontalCards = group.horizontal && Object.values(group.horizontal);
+  if (verticalCards) {
+    // sort the cards
+    // return them in an two dimensional array
+    // starting to think we should store the flipped numbers in the db so we don't have to
+    // perform the calculations/reversing everywhere in the code - we should just look at the flipped boolean
+    return verticalCards
+      .sort((a, b) => parseInt(a.number) - parseInt(b.number))
+      .map((card) => [{ ...card, uprightFor: 'vertical' }]);
+  }
+
+  if (horizontalCards) {
+    return horizontalCards
+      .sort((a, b) => parseInt(a.number) - parseInt(b.number))
+      .map((card) => ({ ...card, uprightFor: 'horizontal' }));
+  }
+
+  // so let's start simple with the two card tests
+  // vertical group
+  // it might be best to return groups with the orientation as the key rather than the card number
+  // but l;et's start like this
+  return [];
 }
