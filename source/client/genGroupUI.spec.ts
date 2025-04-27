a3d78cbd75b8476f4f0b93c0695817efe1dfd59f
Sean Li <9913851+saifahn@users.noreply.github.com>
2025-04-28 00:23:54 +0800

    wip: simple add card to group

--- source/client/genGroupUI.spec.ts
+++ source/client/genGroupUI.spec.ts
@@ -14,37 +14,42 @@ connectingNumsTests(
 
 connectingNumsTests.run();
 
-// const connectCardToGroupTests = suite('connectCardToGroup');
+const connectCardToGroupTests = suite('connectCardToGroup');
 
-// connectCardToGroupTests(
-//   'should connect a greater card to the bottom of a group of 2 cards',
-//   () => {
-//     const group: Group = {
-//       number: 2,
-//       0: [
-//         { id: '1', lowNum: '01', uprightFor: 'vertical' },
-//         { id: '2', lowNum: '02', uprightFor: 'vertical' },
-//       ],
-//     };
-//     const res = connectCardToGroup({
-//       group,
-//       card: {
-//         id: '3',
-//         lowNum: '03',
-//         uprightFor: 'vertical',
-//       },
-//       orientation: 'vertical',
-//     });
+connectCardToGroupTests(
+  'should connect a greater card to the bottom of a group of 2 cards',
+  () => {
+    const group: Group = {
+      number: 2,
+      0: [
+        { id: '1', lowNum: '01', uprightFor: 'vertical' },
+        { id: '2', lowNum: '02', uprightFor: 'vertical' },
+      ],
+    };
+    connectCardToGroup({
+      group,
+      card: {
+        id: '3',
+        lowNum: '03',
+        uprightFor: 'vertical',
+      },
+      orientation: 'vertical',
+      connectingCard: {
+        id: '2',
+        lowNum: '02',
+        uprightFor: 'vertical',
+      },
+    });
 
-//     assert.equal(res, {
-//       number: 2,
-//       0: [
-//         { id: '1', lowNum: '01', uprightFor: 'vertical' },
-//         { id: '2', lowNum: '02', uprightFor: 'vertical' },
-//         { id: '3', lowNum: '03', uprightFor: 'vertical' },
-//       ],
-//     });
-//   }
-// );
+    assert.equal(group, {
+      number: 2,
+      0: [
+        { id: '1', lowNum: '01', uprightFor: 'vertical' },
+        { id: '2', lowNum: '02', uprightFor: 'vertical' },
+        { id: '3', lowNum: '03', uprightFor: 'vertical' },
+      ],
+    });
+  }
+);
 
-// connectCardToGroupTests.run();
+connectCardToGroupTests.run();
